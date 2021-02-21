009cea1e0801dc95b3de55a615ef9dd14de169e3
Grant Duan <duanxiugang@sina.com>
2021-02-21 18:08:15 +0800

    add comments

--- GeneratorsTest.js
+++ GeneratorsTest.js
@@ -16,6 +16,7 @@ console.log(r1);
 
 
 /*
+//only work in browser
 fetch("https://jsonplaceholder.typicode.com/posts/1").then(x => console.log(x));
 console.log("1");
 fetch("https://jsonplaceholder.typicode.com/posts/2").then(x => console.log(x));
